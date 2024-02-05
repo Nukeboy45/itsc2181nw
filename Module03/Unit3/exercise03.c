e03204a38c6b39fc625b20b390a87b3090e68dbd
NathanWeisskopf <nathan@goes211.com>
2024-02-05 18:25:49 -0500

    Completed exercise 3, renamed old exercise 3 to exercise 5

--- Module03/Unit3/exercise03.c
+++ Module03/Unit3/exercise03.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
-#include <math.h>
-#define e 2.718282
-#define logBase10e 0.434294
 
-int main()
-{   
-    printf("%f\n", e);
-    printf("%f\n", logBase10e);
-    return 0;
+struct box {
+    int itemnum;
+    char* color;
+    int height;
+    int width;
+    int depth;
+    float x;
+    float y;
+    float z;
+};
+
+int main() {
+    struct box boxArr[100];
+
+    boxArr[0].itemnum = 1;
+    boxArr[0].color = "red";
+    boxArr[0].height = 1;
+    boxArr[0].width = 1;
+    boxArr[0].depth = 1;
+    boxArr[0].x = 0;
+    boxArr[0].y = 0;
+    boxArr[0].z = 0;
+
+    boxArr[1].itemnum = 2;
+    boxArr[1].color = "blue";
+    boxArr[1].height = 2;
+    boxArr[1].width = 2;
+    boxArr[1].depth = 2;
+    boxArr[1].x = 1;
+    boxArr[1].y = 1;
+    boxArr[1].z = 1;
 }
